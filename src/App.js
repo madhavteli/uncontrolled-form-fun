cb9473a29e0fa9eee60d9bba88f7aaa1e254e15e
mteli <mteli@xento.com>
2021-08-11 08:17:28 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, {useState} from 'react';
-import Btn from "./Components/Btn";
+import React from 'react';
+import ControlledForm from "./Components/ControlledForm";
 
 function App() {
 
-	const [count, setCount] =useState(0);
-
-	const increaseCount = () => {
-		setCount(count+1);
-	}
-
-	const decreaseCount = () => {
-		setCount(count <= 0 ? count : count-1);
-	}
-
-	const resetCount = () => {
-		setCount(0);
-	}
-
 	return (
 		<div>
-			<Btn btnText='+' onClick={increaseCount}/>
-			<h1>{count}</h1>
-			<Btn btnText='-' onClick={decreaseCount}/>
-			<Btn btnText='Reset' onClick={resetCount}/>
+			<ControlledForm/>
 		</div>
 	);
 }
